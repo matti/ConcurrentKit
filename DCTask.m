9846eb31db18fc0f86adfa43ea8ec9f364c361a4
Dalton Cherry <daltoniam@gmail.com>
2014-05-02 14:42:15 -0700

    added task source

---
+++ DCTask.m
@@ -0,0 +1,147 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  DCTask.m
+//
+//  Created by Dalton Cherry on 5/2/14.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#import "DCTask.h"
+
+@interface DCTask ()
+
+//This basically a singly linked list
+@property(nonatomic,strong)DCTask *next;
+
+//store the work to do
+@property(nonatomic,strong)DCTask*(^work)(id);
+@property(nonatomic,strong)DCAsyncTask asyncTask;
+@property(nonatomic,assign)BOOL isMain;
+@property(nonatomic,strong)void(^errorHandler)(id);
+
+@end
+
+@implementation DCTask
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)start
+{
+    [self runTask:self param:nil];
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)runTask:(DCTask*)task param:(id)param
+{
+    if(task.asyncTask)
+    {
+        task.asyncTask(^(id val){
+            dispatch_async(dispatch_get_main_queue(),^{
+                [self finishedTask:task result:val];
+            });
+        },^(NSError *error){
+            [self processError:task val:error];
+        });
+    }
+    else if(task.work)
+    {
+        if(task.isMain)
+        {
+            id val = task.work(param);
+            [self finishedTask:task result:val];
+        }
+        else
+        {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                id val = task.work(param);
+                dispatch_async(dispatch_get_main_queue(),^{
+                    [self finishedTask:task result:val];
+                });
+            });
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(BOOL)processError:(DCTask*)currentTask val:(id)value
+{
+    if([value isKindOfClass:[NSError class]])
+    {
+        if(!self.errorHandler)
+            self.errorHandler = currentTask.errorHandler;
+        if(!self.errorHandler)
+        {
+            DCTask *task = self;
+            while(!self.errorHandler)
+            {
+                self.errorHandler = task.next.errorHandler;
+                task = task.next;
+            }
+        }
+        
+        if(self.errorHandler)
+            self.errorHandler(value);
+        return YES;
+    }
+    return NO;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)finishedTask:(DCTask*)task result:(id)result
+{
+    if([self processError:task val:result])
+        return;
+    //a task was returned, so let's add it into the chain
+    if([result isKindOfClass:[DCTask class]])
+    {
+        DCTask *rTask = result;
+        rTask.next = task.next;
+        task.next = rTask;
+    }
+    [self runTask:task.next param:result];
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(DCTask*(^)(id))begin
+{
+    //__weak id weakSelf = self;
+    return ^(DCTask*(^begin)(id)){
+        DCTask *task = [DCTask new];
+        self.next = task;
+        self.work = begin;
+        return task;
+    };
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(DCTask*(^)(id))then
+{
+    return ^(DCTask*(^work)(id)){
+        DCTask *task = [DCTask new];
+        self.next = task;
+        self.work = work;
+        return task;
+    };
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(DCTask*(^)(id))thenMain
+{
+    return ^(DCTask*(^work)(id)){
+        DCTask *task = [DCTask new];
+        self.isMain = YES;
+        self.next = task;
+        self.work = work;
+        return task;
+    };
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+-(void(^)(id))catch
+{
+    return ^(void(^error)(NSError*)){
+        self.errorHandler = error;
+    };
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
++(DCTask*)newAsyncTask:(DCAsyncTask)asyncTask
+{
+    DCTask *task = [DCTask new];
+    task.asyncTask = asyncTask;
+    return task;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@end
--- README.md
+++ README.md
@@ -2,3 +2,21 @@ ConcurrentKit
 =============
 
 Concurrency made easy with promise/future like syntax for OS X and iOS. 
+
+## More to come.... ##
+
+
+## Requirements ##
+
+ConcurrentKit requires at least iOS 6 or OS X 10.8.
+
+
+## License ##
+
+ConcurrentKit is license under the Apache License.
+
+## Contact ##
+
+### Dalton Cherry ###
+* https://github.com/daltoniam
+* http://twitter.com/daltoniam
